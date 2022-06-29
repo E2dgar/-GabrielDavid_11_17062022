dafd194434220f6a75c204c6d16c94b4d9c84c28
DG <gdavidcefim@gmail.com>
2022-06-29 13:02:06 +0200

    JS doc

--- src/services/renderDataState.jsx
+++ src/services/renderDataState.jsx
@@ -1,6 +1,11 @@
 import Error from '../components/errorData';
 import Loader from '../components/loader';
 
+/**
+ * Return error component , loading component or True
+ * @param {Object} data From fetch
+ * @returns
+ */
 const renderData = (data) => {
     if (data) {
         return data.error ? <Error /> : data.isLoading ? <Loader /> : true;
