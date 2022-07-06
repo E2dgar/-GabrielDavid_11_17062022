9774e4add08e9df95be66b51ff5433019fb1cef7
DG <gdavidcefim@gmail.com>
2022-07-06 12:53:15 +0200

    Slider functions

--- src/containers/Slider/index.jsx
+++ src/containers/Slider/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import SlideButton from '../../atoms/SlideButton/SlideButton';
 import SlideIndex from '../../atoms/SlideIndex';
 
 const StyledSlider = styled.section`
@@ -7,6 +8,7 @@ const StyledSlider = styled.section`
     max-width: 1240px;
     height: 415px;
     overflow: hidden;
+    position: relative;
 
     div {
         width: 300%;
@@ -28,17 +30,36 @@ const StyledSlider = styled.section`
 const Slider = ({ content }) => {
     const [index, setIndex] = useState(0);
 
-    const handlePrev = () => {};
+    const handleNext = () => {
+        if (index === content.length - 1) {
+            setIndex(0);
+        } else {
+            setIndex(index + 1);
+        }
+    };
 
-    const handleNext = () => {};
+    const handlePrev = () => {
+        if (index === 0) {
+            setIndex(content.length - 1);
+        } else {
+            setIndex(index - 1);
+        }
+    };
+
+    const handleClick = {
+        prev: handlePrev,
+        next: handleNext
+    };
 
     return (
         <StyledSlider>
+            <SlideButton direction="prev" handleClick={handleClick} />
+
             <div className="slides-container">
                 <img src={content?.[index]} alt={`Accomodation-${index}`} />
-
-                <SlideIndex handlePrev={handlePrev} handleNext={handleNext} />
             </div>
+
+            <SlideButton direction="next" handleClick={handleClick} />
         </StyledSlider>
     );
 };
